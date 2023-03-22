f617f3b2481ab19ca479f79b29ae8a31ad3c7e02
Andrea Bizzotto <codewithandrea@gmail.com>
2023-03-22 13:46:21 +0000

    Implement ImageUploadService
    # Conflicts:
    #	ecommerce_app/lib/src/features/products_admin/presentation/admin_product_upload_controller.g.dart
    
    # Conflicts:
    #	ecommerce_app/lib/src/features/products_admin/presentation/admin_product_upload_controller.g.dart

---
+++ ecommerce_app/lib/src/features/products_admin/application/image_upload_service.dart
@@ -0,0 +1,29 @@
+import 'package:ecommerce_app/src/features/products/data/products_repository.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:ecommerce_app/src/features/products_admin/data/image_upload_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'image_upload_service.g.dart';
+
+class ImageUploadService {
+  const ImageUploadService(this.ref);
+  final Ref ref;
+
+  Future<void> uploadProduct(Product product) async {
+    // upload to storage and return download URL
+    final downloadUrl = await ref
+        .read(imageUploadRepositoryProvider)
+        .uploadProductImageFromAsset(product.imageUrl, product.id);
+
+    // write to Cloud Firestore
+    await ref
+        .read(productsRepositoryProvider)
+        .createProduct(product.id, downloadUrl);
+  }
+}
+
+@riverpod
+ImageUploadService imageUploadService(ImageUploadServiceRef ref) {
+  return ImageUploadService(ref);
+}
---
+++ ecommerce_app/lib/src/features/products_admin/application/image_upload_service.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'image_upload_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$imageUploadServiceHash() =>
+    r'2e2d4b581424f692b3175fa98f71374de9f05b0b';
+
+/// See also [imageUploadService].
+@ProviderFor(imageUploadService)
+final imageUploadServiceProvider =
+    AutoDisposeProvider<ImageUploadService>.internal(
+  imageUploadService,
+  name: r'imageUploadServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$imageUploadServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ImageUploadServiceRef = AutoDisposeProviderRef<ImageUploadService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
