682bc0ba0a4e2f00fe20775c48719bf8101f7820
Andrea Bizzotto <codewithandrea@gmail.com>
2023-03-22 13:46:21 +0000

    Implement ImageUploadService

---
+++ ecommerce_app/lib/src/features/products_admin/application/image_upload_service.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'image_upload_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$imageUploadServiceHash() =>
+    r'2e2d4b581424f692b3175fa98f71374de9f05b0b';
+
+/// See also [imageUploadService].
+@ProviderFor(imageUploadService)
+final imageUploadServiceProvider =
+    AutoDisposeProvider<ImageUploadService>.internal(
+  imageUploadService,
+  name: r'imageUploadServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$imageUploadServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ImageUploadServiceRef = AutoDisposeProviderRef<ImageUploadService>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
