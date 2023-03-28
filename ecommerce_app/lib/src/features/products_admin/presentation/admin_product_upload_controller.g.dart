f94585c05bbb5423acb26218f98e6135f7619109
Andrea Bizzotto <codewithandrea@gmail.com>
2023-03-28 11:28:49 +0100

    Draft AdminProductUploadController

---
+++ ecommerce_app/lib/src/features/products_admin/presentation/admin_product_upload_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'admin_product_upload_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$adminProductUploadControllerHash() =>
+    r'9109084491fdab69a7e67732bfb04727bd8c2d1b';
+
+/// See also [AdminProductUploadController].
+@ProviderFor(AdminProductUploadController)
+final adminProductUploadControllerProvider = AutoDisposeAsyncNotifierProvider<
+    AdminProductUploadController, void>.internal(
+  AdminProductUploadController.new,
+  name: r'adminProductUploadControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$adminProductUploadControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AdminProductUploadController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
