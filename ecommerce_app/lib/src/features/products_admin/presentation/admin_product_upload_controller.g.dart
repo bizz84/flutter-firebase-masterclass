7ce2adbebba444462773b3ceb687a6eb2653a8b8
Andrea Bizzotto <codewithandrea@gmail.com>
2023-03-28 12:56:09 +0100

    Update AdminProductUploadController

--- ecommerce_app/lib/src/features/products_admin/presentation/admin_product_upload_controller.g.dart
+++ ecommerce_app/lib/src/features/products_admin/presentation/admin_product_upload_controller.g.dart
@@ -7,7 +7,7 @@ part of 'admin_product_upload_controller.dart';
 // **************************************************************************
 
 String _$adminProductUploadControllerHash() =>
-    r'9109084491fdab69a7e67732bfb04727bd8c2d1b';
+    r'd0ee7f595ae5a51251d2ebe218e01f94a2b0adc3';
 
 /// See also [AdminProductUploadController].
 @ProviderFor(AdminProductUploadController)
@@ -24,4 +24,4 @@ final adminProductUploadControllerProvider = AutoDisposeAsyncNotifierProvider<
 
 typedef _$AdminProductUploadController = AutoDisposeAsyncNotifier<void>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
