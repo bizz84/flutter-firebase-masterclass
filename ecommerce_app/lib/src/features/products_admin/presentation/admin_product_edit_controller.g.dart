f68a3632c2465d759de8990bb22ac3f13b3ce99a
Andrea Bizzotto <codewithandrea@gmail.com>
2023-03-30 11:58:34 +0100

    Implement AdminProductEditController

---
+++ ecommerce_app/lib/src/features/products_admin/presentation/admin_product_edit_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'admin_product_edit_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$adminProductEditControllerHash() =>
+    r'b06a2f48c57572efc268e8cab568d1edabd0e16d';
+
+/// See also [AdminProductEditController].
+@ProviderFor(AdminProductEditController)
+final adminProductEditControllerProvider =
+    AutoDisposeAsyncNotifierProvider<AdminProductEditController, void>.internal(
+  AdminProductEditController.new,
+  name: r'adminProductEditControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$adminProductEditControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AdminProductEditController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- ecommerce_app/lib/src/features/products_admin/presentation/admin_product_edit_screen.dart
+++ ecommerce_app/lib/src/features/products_admin/presentation/admin_product_edit_screen.dart
@@ -10,8 +10,10 @@ import 'package:ecommerce_app/src/constants/app_sizes.dart';
 import 'package:ecommerce_app/src/features/products/data/products_repository.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:ecommerce_app/src/features/products_admin/data/template_products_providers.dart';
+import 'package:ecommerce_app/src/features/products_admin/presentation/admin_product_edit_controller.dart';
 import 'package:ecommerce_app/src/features/products_admin/presentation/product_validator.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_app/src/utils/async_value_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -112,40 +114,37 @@ class _AdminProductScreenContentsState
 
   Future<void> _submit() async {
     if (_formKey.currentState!.validate()) {
-      await showAlertDialog(
-        context: context,
-        title: 'Not implemented'.hardcoded,
-      );
-      // TODO: Uncomment
-      // final scaffoldMessenger = ScaffoldMessenger.of(context);
-      // await ref.read(adminProductEditControllerProvider.notifier).updateProduct(
-      //       product: product,
-      //       title: _titleController.text,
-      //       description: _descriptionController.text,
-      //       price: _priceController.text,
-      //       availableQuantity: _availableQuantityController.text,
-      //     );
-      // // Inform the user that the product has been updated
-      // scaffoldMessenger.showSnackBar(
-      //   SnackBar(
-      //     content: Text(
-      //       'Product updated'.hardcoded,
-      //     ),
-      //   ),
-      // );
+      final scaffoldMessenger = ScaffoldMessenger.of(context);
+      final success = await ref
+          .read(adminProductEditControllerProvider.notifier)
+          .updateProduct(
+            product: product,
+            title: _titleController.text,
+            description: _descriptionController.text,
+            price: _priceController.text,
+            availableQuantity: _availableQuantityController.text,
+          );
+      if (success) {
+        // Inform the user that the product has been updated
+        scaffoldMessenger.showSnackBar(
+          SnackBar(
+            content: Text(
+              'Product updated'.hardcoded,
+            ),
+          ),
+        );
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Uncomment
-    // ref.listen<AsyncValue>(
-    //   adminProductEditControllerProvider,
-    //   (_, state) => state.showAlertDialogOnError(context),
-    // );
-    // final state = ref.watch(adminProductEditControllerProvider);
-    // final isLoading = state.isLoading;
-    const isLoading = false;
+    ref.listen<AsyncValue>(
+      adminProductEditControllerProvider,
+      (_, state) => state.showAlertDialogOnError(context),
+    );
+    final state = ref.watch(adminProductEditControllerProvider);
+    final isLoading = state.isLoading;
     const autovalidateMode = AutovalidateMode.disabled;
     return Scaffold(
       appBar: AppBar(
