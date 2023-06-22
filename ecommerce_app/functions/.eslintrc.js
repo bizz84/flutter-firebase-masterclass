066416611ce5342e088bd0cb325a227becafca99
Andrea Bizzotto <codewithandrea@gmail.com>
2023-06-22 13:44:50 +0100

    Updated .eslintrc.js, skipLibCheck in tsconfig.json

--- ecommerce_app/functions/.eslintrc.js
+++ ecommerce_app/functions/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   parserOptions: {
     project: ["tsconfig.json", "tsconfig.dev.json"],
     sourceType: "module",
+    tsconfigRootDir: __dirname,
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
@@ -29,5 +30,16 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "semi": ["off"],
+    "require-jsdoc": ["off"],
+    "object-curly-spacing": ["off"],
+    "spaced-comment": ["off"],
+    "max-len": [
+      "error",
+      {
+        "code": 140,
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["off"],
   },
 };
