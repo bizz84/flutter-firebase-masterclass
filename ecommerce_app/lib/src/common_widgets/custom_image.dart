a16901142a2506906ee9ea2b4c0b2c572d8813fd
Andrea Bizzotto <codewithandrea@gmail.com>
2023-07-19 10:15:42 +0100

    Add `localhostFriendlyImageUrl` to CustomImage

--- ecommerce_app/lib/src/common_widgets/custom_image.dart
+++ ecommerce_app/lib/src/common_widgets/custom_image.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 /// Custom image widget that loads an image as a static asset or uses
@@ -14,8 +17,34 @@ class CustomImage extends StatelessWidget {
     return AspectRatio(
       aspectRatio: 1,
       child: imageUrl.startsWith('http')
-          ? CachedNetworkImage(imageUrl: imageUrl)
+          ? CachedNetworkImage(imageUrl: localhostFriendlyImageUrl)
           : Image.asset(imageUrl),
     );
+  }
+
+  /// Unless the correct IP is used, the Android Emulator will fail to load
+  /// localhost images that were saved with the Firebase Local Emulator
+  /// This code ensures the correct IP is used on localhost
+  String get localhostFriendlyImageUrl {
+    const localhostDefault1 = 'http://127.0.0.1';
+    const localhostDefault2 = 'http://localhost';
+    const localhostAndroid = 'http://10.0.2.2';
+    // To prevent an exception, check kIsWeb before using the [Platform] class
+    final isAndroid = !kIsWeb && Platform.isAndroid;
+    if (isAndroid) {
+      // * Android emulator can only connect to localhost with the 10.0.2.2 IP:
+      // * https://stackoverflow.com/a/5806384/436422
+      if (imageUrl.startsWith(localhostDefault1)) {
+        return imageUrl.replaceFirst(localhostDefault1, localhostAndroid);
+      } else if (imageUrl.startsWith(localhostDefault2)) {
+        return imageUrl.replaceFirst(localhostDefault2, localhostAndroid);
+      }
+    }
+    if (!isAndroid && imageUrl.startsWith(localhostAndroid)) {
+      // * If the image was originally saved with the Android emulator, but
+      // * we're now try to load it from a different platform, use 127.0.0.1:
+      return imageUrl.replaceFirst(localhostAndroid, localhostDefault1);
+    }
+    return imageUrl;
   }
 }
