88704b3450db6125a132198ab7bcee469014b98b
Andrea Bizzotto <codewithandrea@gmail.com>
2023-09-18 12:04:03 +0100

    Add env.dart & .gitignore, update Android files for Stripe integration, Add AppBootstrapStripe during startup
    # Conflicts:
    #	ecommerce_app/lib/main.dart
    
    # Conflicts:
    #	ecommerce_app/android/app/src/main/kotlin/com/example/ecommerce_app/MainActivity.kt
    
    # Conflicts:
    #	ecommerce_app/android/app/src/main/kotlin/com/example/ecommerce_app/MainActivity.kt
    #	ecommerce_app/android/settings.gradle

--- ecommerce_app/android/app/src/main/kotlin/com/example/ecommerce_app/MainActivity.kt
+++ ecommerce_app/android/app/src/main/kotlin/com/example/ecommerce_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ecommerce_app
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
