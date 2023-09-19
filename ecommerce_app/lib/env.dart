43df0abbe8eb9a6edc58759d2b52b04454fbaa54
Andrea Bizzotto <codewithandrea@gmail.com>
2023-09-19 11:03:46 +0100

    Add instructions to set STRIPE_PUBLISHABLE_KEY

--- ecommerce_app/lib/env.dart
+++ ecommerce_app/lib/env.dart
@@ -2,6 +2,11 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
+// * To configure this, create the `ecommerce_app/.env` file and add your key:
+// STRIPE_PUBLISHABLE_KEY=pk_test_YOUR_STRIPE_PUBLISHABLE_KEY
+// * Then, run the generator:
+// dart run build_runner build -d
+
 @Envied()
 final class Env {
   @EnviedField(varName: 'STRIPE_PUBLISHABLE_KEY', obfuscate: true)
