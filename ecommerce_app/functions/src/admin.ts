6c589662ae406c88852db74ed6f11eb688acc9d9
Andrea Bizzotto <codewithandrea@gmail.com>
2023-09-22 14:59:58 +0100

    Add onStripeProductWritten, onStripePriceWritten, onStripeCustomerCreated triggers
    # Conflicts:
    #	ecommerce_app/functions/package-lock.json
    
    # Conflicts:
    #	ecommerce_app/functions/package-lock.json
    #	ecommerce_app/functions/package.json
    #	ecommerce_app/functions/src/index.ts
    
    # Conflicts:
    #	ecommerce_app/functions/package-lock.json

---
+++ ecommerce_app/functions/src/admin.ts
@@ -0,0 +1,33 @@
+import * as admin from "firebase-admin"
+import * as logger from "firebase-functions/logger"
+import * as firestore from "@google-cloud/firestore"
+
+export async function makeAdminIfWhitelisted(user: admin.auth.UserRecord) {
+  const email = user.email
+  if (email === undefined) {
+    logger.log(`User ${user.uid} doesn't have an email address`)
+    return
+  }
+  // * disabled to make testing easier with email & password auth
+  // if (!user.emailVerified) {
+  //   logger.log(`${email} is not verified`)
+  //   return
+  // }
+  if (!email.endsWith("@codewithandrea.com")) {
+    logger.log(`${email} doesn't belong to a whitelisted domain`)
+    return
+  }
+  if (user.customClaims?.admin === true) {
+    logger.log(`${email} is already an admin`)
+    return
+  }
+  // set custom claim
+  await admin.auth().setCustomUserClaims(user.uid, {
+    admin: true,
+  })
+  // write to Firestore so the client knows it needs to update
+  await admin.firestore().doc(`metadata/${user.uid}`).set({
+    refreshTime: firestore.FieldValue.serverTimestamp(),
+  })
+  logger.log(`Custom claim set! ${email} is now an admin`)
+}
