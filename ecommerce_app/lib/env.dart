f057fd30058da46e43cf011a013030b09e6cb8fc
Andrea Bizzotto <codewithandrea@gmail.com>
2023-09-26 08:44:12 +0100

    Update README with Stripe instructions
    # Conflicts:
    #	ecommerce_app/README.md

--- ecommerce_app/lib/env.dart
+++ ecommerce_app/lib/env.dart
@@ -2,9 +2,12 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-// * To configure this, create the `ecommerce_app/.env` file and add your key:
+// * To configure this, follow these steps.
+// * 1. grab an API key from here:
+// * https://dashboard.stripe.com/test/apikeys
+// * 2. create a `.env` file and add your key:
 // STRIPE_PUBLISHABLE_KEY=pk_test_YOUR_STRIPE_PUBLISHABLE_KEY
-// * Then, run the generator:
+// * 3. run the generator:
 // dart run build_runner build -d
 
 @Envied()
