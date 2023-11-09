92a15e8e29f9c18fd16d3025f05a1e852c6c3406
Andrea Bizzotto <codewithandrea@gmail.com>
2023-11-09 11:48:51 +0000

    Implement ReviewsRepository, update security rules

--- ecommerce_app/lib/src/features/reviews/data/reviews_repository.dart
+++ ecommerce_app/lib/src/features/reviews/data/reviews_repository.dart
@@ -10,30 +10,37 @@ class ReviewsRepository {
   const ReviewsRepository(this._firestore);
   final FirebaseFirestore _firestore;
 
+  static String reviewPath(ProductID id, UserID uid) =>
+      'products/$id/reviews/$uid';
+  static String reviewsPath(ProductID id) => 'products/$id/reviews';
+
   /// Single review for a given product given by a specific user
   /// Emits non-null values if the user has reviewed the product
   Stream<Review?> watchUserReview(ProductID id, UserID uid) {
-    // TODO: Implement
-    return Stream.value(null);
+    final ref = _reviewRef(id, uid);
+    return ref.snapshots().map((snapshot) => snapshot.data());
   }
 
   /// Single review for a given product given by a specific user
   /// Returns a non-null value if the user has reviewed the product
-  Future<Review?> fetchUserReview(ProductID id, UserID uid) {
-    // TODO: Implement
-    return Future.value(null);
+  Future<Review?> fetchUserReview(ProductID id, UserID uid) async {
+    final ref = _reviewRef(id, uid);
+    final snapshot = await ref.get();
+    return snapshot.data();
   }
 
   /// All reviews for a given product from all users
   Stream<List<Review>> watchReviews(ProductID id) {
-    // TODO: Implement
-    return Stream.value([]);
+    final ref = _reviewsRef(id);
+    return ref.snapshots().map((snapshot) =>
+        snapshot.docs.map((docSnapshot) => docSnapshot.data()).toList());
   }
 
   /// All reviews for a given product from all users
-  Future<List<Review>> fetchReviews(ProductID id) {
-    // TODO: Implement
-    return Future.value([]);
+  Future<List<Review>> fetchReviews(ProductID id) async {
+    final ref = _reviewsRef(id);
+    final snapshot = await ref.get();
+    return snapshot.docs.map((docSnapshot) => docSnapshot.data()).toList();
   }
 
   /// Submit a new review or update an existing review for a given product
@@ -45,9 +52,24 @@ class ReviewsRepository {
     required UserID uid,
     required Review review,
   }) {
-    // TODO: Implement
-    throw UnimplementedError();
+    final ref = _reviewRef(productId, uid);
+    return ref.set(review);
   }
+
+  DocumentReference<Review?> _reviewRef(ProductID id, UserID uid) =>
+      _firestore.doc(reviewPath(id, uid)).withConverter(
+            fromFirestore: (doc, _) =>
+                doc.exists ? Review.fromMap(doc.data()!) : null,
+            toFirestore: (review, _) => review != null ? review.toMap() : {},
+          );
+
+  Query<Review> _reviewsRef(ProductID id) => _firestore
+      .collection(reviewsPath(id))
+      .orderBy('date', descending: true)
+      .withConverter(
+        fromFirestore: (doc, _) => Review.fromMap(doc.data()!),
+        toFirestore: (review, _) => review.toMap(),
+      );
 }
 
 @Riverpod(keepAlive: true)
