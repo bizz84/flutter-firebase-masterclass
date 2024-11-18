7459d4b47469eacf8f47fae83989e1f068524718
Andrea Bizzotto <codewithandrea@gmail.com>
2024-11-18 14:10:16 +0000

    Initial Firebase setup with flutterfire + add firebase_core, firebase_auth packages, platform configuration on Android, iOS, macOS
    # Conflicts:
    #	ecommerce_app/android/app/build.gradle.kts
    #	ecommerce_app/android/settings.gradle
    #	ecommerce_app/ios/Podfile.lock
    #	ecommerce_app/ios/Runner.xcodeproj/project.pbxproj
    #	ecommerce_app/macos/Podfile.lock
    #	ecommerce_app/pubspec.lock
    #	ecommerce_app/pubspec.yaml

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:ecommerce_app/firebase_options.dart';
 import 'package:ecommerce_app/src/app_bootstrap_firebase.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/app_bootstrap.dart';
 // ignore:depend_on_referenced_packages
@@ -6,7 +8,9 @@ import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // TODO: Initialize firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // turn off the # in the URLs on the web
   usePathUrlStrategy();
   // create an app bootstrap instance
--- ecommerce_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ecommerce_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_auth
+import firebase_core
 import path_provider_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
