9865c1769f58ec1dd5c5f8eb00701b8b95fa4834
Andrea Bizzotto <codewithandrea@gmail.com>
2025-04-18 12:46:41 +0100

    Remove unused import

--- ecommerce_app/lib/src/features/cart/data/local/sembast_cart_repository.dart
+++ ecommerce_app/lib/src/features/cart/data/local/sembast_cart_repository.dart
@@ -2,7 +2,6 @@ import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository
 import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
 import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart';
 import 'package:sembast_web/sembast_web.dart';
 
